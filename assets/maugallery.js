f4d4a6ebaa7db37419589722fb137db06bf8acce
Alhassane Seye <seyealhassane@gmail.com>
2023-05-29 22:09:47 +0200

    ajout

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -224,6 +224,7 @@
       $.each(tags, function(index, value) {
         tagItems += `<li class="nav-item active">
                 <span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
+
       });
       var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
 
